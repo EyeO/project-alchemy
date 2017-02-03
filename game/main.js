13a30b5109cb24719916d1f916c9d5ae31a1272d
MorrisTheHorris <morristhe1horris@gmail.com>
2017-02-03 12:03:33 +1100

    Began writing Machine object structure

--- game/main.js
+++ game/main.js
@@ -1,11 +1,37 @@
 $(function() {
     var credits = 0,
-        matterRate = 1,
+        matterRate = 0,
         matter = 0,
         energy = 0;
 
-    setInterval(function(){
-      matter+=matterRate;
-      $('.current-matter').html(matter);
-    },1000);
+    function inherit(proto) {
+      function F() {}
+      F.prototype = proto;
+      return new F();
+    }
+
+    function Machine(cost){
+      this.cost = cost;
+      this.number = 0;
+    }
+
+    Machine.prototype.numOfMachines = function() {
+        alert(this.number);
+    };
+
+    Machine.prototype.purchase = function(num) {
+        this.number += num;
+    };
+
+    function energyCollector(cost, rate){
+        Machine.call(this, cost);
+        this.rate = rate;
+    }
+    energyCollector.prototype = inherit(Machine.prototype);
+
+    var solarPanel = new energyCollector(1000,100);
+    //solarPanel.purchase(3);
+    //solarPanel.purchase(7);
+    //solarPanel.numOfMachines();
+
 });
