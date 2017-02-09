3a184f23b46bced318996075573d067b82ac8157
MorrisTheHorris <morristhe1horris@gmail.com>
2017-02-09 11:25:00 +1100

    Added Machines object to manage all other Machine objects

--- game/main.js
+++ game/main.js
@@ -4,34 +4,79 @@ $(function() {
         matter = 0,
         energy = 0;
 
+    //http://javascript.info/tutorial/inheritance
     function inherit(proto) {
-      function F() {}
-      F.prototype = proto;
-      return new F();
+        function F() {}
+        F.prototype = proto;
+        return new F();
     }
 
-    function Machine(cost){
-      this.cost = cost;
-      this.number = 0;
+    //Declare machine class
+    function Machine(id, cost, name) {
+        this.id = id;
+        this.cost = cost;
+        this.instances = 0;
+        this.name = name;
+
+        this.purchase = function(num) {
+            this.instances += num;
+        };
     }
 
-    Machine.prototype.numOfMachines = function() {
-        alert(this.number);
-    };
-
-    Machine.prototype.purchase = function(num) {
-        this.number += num;
-    };
-
-    function energyCollector(cost, rate){
-        Machine.call(this, cost);
+    //Declare energy collector class
+    function EnergyCollector(id, cost, rate, name) {
+        Machine.call(this, id, cost, name);
         this.rate = rate;
-    }
-    energyCollector.prototype = inherit(Machine.prototype);
 
-    var solarPanel = new energyCollector(1000,100);
-    //solarPanel.purchase(3);
-    //solarPanel.purchase(7);
-    //solarPanel.numOfMachines();
+        this.update = function(deltaTime) {
+            alert(deltaTime * this.instances * this.rate / 1000);
+        };
+    }
+
+    //Energy collector inherits from machine
+    EnergyCollector.prototype = inherit(Machine.prototype);
+
+    //http://stackoverflow.com/a/1248023
+    //Keep track of all machines
+    Machines = {
+        newEnergyCollector: function() {
+            var newMachine = {};
+            EnergyCollector.apply(newMachine, arguments);
+            this.allMachines.push(newMachine);
+            return newMachine;
+        },
+
+        allMachines: [],
+
+        getMachineByID: function(id) {
+            for (var i = 0; i < this.allMachines.length; i++) {
+                if (this.allMachines[i].id == id) {
+                    return this.allMachines[i];
+                }
+            }
+        },
+
+        runFunction: function(fn) {
+            for (var i = 0; i < this.allMachines.length; i++) {
+                fn.call(this.allMachines[i]);
+            }
+        },
+
+        update: function(deltaTime) {
+            for (var i = 0; i < this.allMachines.length; i++) {
+                this.allMachines[i].update(deltaTime);
+            }
+        }
+    };
+
+    //Declare solar panels
+    Machines.newEnergyCollector(1, 1000, 100, 'Tier 1 Solar Panel');
+    Machines.newEnergyCollector(2, 1000000, 50000, 'Tier 2 Solar Panel');
+
+    //Purchase 3 solar panels
+    Machines.getMachineByID(1).purchase(3);
+
+    //Update values
+    Machines.update(50);
 
 });
