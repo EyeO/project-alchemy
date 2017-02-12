017bb3116e683fbfbb7325a2f379824afd081036
MorrisTheHorris <morristhe1horris@gmail.com>
2017-02-12 18:16:54 +1100

    Added more information on machine listing Added credit variables

--- game/main.js
+++ game/main.js
@@ -3,7 +3,7 @@ $.when(
     $.getScript('game/misc.js'),
     $.getScript('game/model/Machine.js'),
     $.getScript('game/model/MachineTypes.js'),
-    $.Deferred(function(deferred){
+    $.Deferred(function(deferred) {
         $(deferred.resolve);
     })
 ).done(function() {
@@ -23,11 +23,12 @@ $.when(
     setInterval(gameLoop, updateInterval);
 
     //Variables
-    var credits = 0,
+    window.credits = 0;
+    var creditsRate = 0,
         matterRate = 0,
         matter = 0,
         energy = 0;
-        energyRate = 0;
+    energyRate = 0;
 
     //Update resources
     function updateResources(delta) {
@@ -35,17 +36,42 @@ $.when(
         energyRate = delta.energy * 1000 / updateInterval;
         matter += delta.matter;
         matterRate = delta.matter * 1000 / updateInterval;
+        window.credits += delta.currency;
+        creditsRate = delta.currency * 1000 / updateInterval;
 
         $('.current-energy').html(formatNumber(energy));
         $('.current-energyRate').html(formatNumber(energyRate));
         $('.current-matter').html(formatNumber(matter));
         $('.current-matterRate').html(formatNumber(matterRate));
+        $('.current-credits').html(formatNumber(window.credits,1));
+        $('.current-creditsRate').html(formatNumber(creditsRate,1));
     }
 
     function updateMachineList() {
         $('.machines ul').empty();
-        Machines.runFunction( function() {
-          $('.machines ul').append('<li class="purchase ' + this.id + '">' + this.name + ' (' + this.type + ') ['+ this.instances + ']</li>');
+        Machines.runFunction(function() {
+            var units;
+            var unary = true;
+            switch (this.type) {
+                case 'collector':
+                    units = ['W'];
+                    break;
+                case 'harvester':
+                    units = ['g/s'];
+                    break;
+                case 'fabricator':
+                    units = ['g/s','W'];
+                    unary = false;
+                    break;
+                case 'generator':
+                    units = ['W','g/s'];
+                    unary = false;
+                    break;
+                default:
+                    throw "Update error: unknown machine type";
+            }
+            var stats = '+' + formatNumber(this.rate) + units[0] + (unary ? '' : (', -' + formatNumber(this.rate2) + units[1] ) );
+            $('.machines ul').append('<li class="purchase ' + this.id + '">' + this.name + ' (' + stats + ') [' + this.instances + ' owned]</li>');
         });
         bindPurchase();
     }
@@ -61,6 +87,8 @@ $.when(
     //Declare
     Machines.newMachine('collector', 0, 100, 1000, 'Solar Panel');
     Machines.newMachine('harvester', 1, 500, 200, 'Mine');
+    Machines.newMachine('generator', 2, 1000000, 1000000, 'Nuclear Reactor', 10);
+    Machines.newMachine('fabricator',3, 10000000, 1000, 'Generic Mass Fabricator', 100000);
 
     updateMachineList();
 
